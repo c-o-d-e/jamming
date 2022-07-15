ddbe4fc06a5d94653b24d214213c90a208113012
c-o-d-e <swapyuk@gmail.com>
2022-07-15 21:34:58 +0100

    add hard coded playlist

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,10 +4,16 @@ import "./TrackList.css";
 import Track from "../Track/Track";
 
 class TrackList extends React.Component {
+  renderMap(){
+    if (this.props.tracks){
+      return this.props.tracks.map(track => {return <Track track={track} key={track.id} />})
+    }
+  }
+
   render() {
     return (
         <div className="TrackList">
-          {this.props.tracks.map(track => <Track track={track} key={track.id}/>)}
+          {this.renderMap()}
         </div>
       );
   }
