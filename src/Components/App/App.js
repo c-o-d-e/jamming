8a6a148979087bd90b43e7af8e40790a048db15a
c-o-d-e <swapyuk@gmail.com>
2022-07-15 12:11:43 +0100

    add minor changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,6 +8,13 @@ import Playlist from "../Playlist/Playlist";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      SearchResults: [
+        { name: "name1", artist: "artist1", album: "album1", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+      ],
+    };
   }
   render() {
     return (
@@ -16,10 +23,10 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component --> */}
+            <SearchResults SearchResults={this.state.SearchResults} />
+            <Playlist />
           </div>
         </div>
       </div>
